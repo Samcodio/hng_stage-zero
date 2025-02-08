dc6438f5bed48f6ab77f71c8cf04d279d18eb4aa
Samcodio <nosikesamuel1@gmail.com>
2025-02-08 14:42:00 -0800

    Initial commit - FastAPI project

---
+++ data.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from datetime import datetime
+import pytz
+from fastapi.responses import PlainTextResponse
+from middleware import setup_cors
+
+
+app = FastAPI()
+setup_cors(app)
+
+@app.get("/", response_class=PlainTextResponse)
+def get_info():
+    return (
+        f"email: nosikesamuel1@gmail.com\n"
+        f"current_time: {datetime.now(pytz.utc).isoformat()}\n"
+        f"github_url: https://github.com/Samcodio/hng_stage-zero"
+    )
+
+

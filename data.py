2e87755675681b046c76e5c5f3c80a4872849c31
Sam_El <146867369+Samcodio@users.noreply.github.com>
2025-02-09 17:01:35 +0100

    Update data.py

--- data.py
+++ data.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI
 from datetime import datetime
 import pytz
-from fastapi.responses import PlainTextResponse
 from middleware import setup_cors
 
 
 app = FastAPI()
 setup_cors(app)
 
-@app.get("/", response_class=PlainTextResponse)
+@app.get("/")
 def get_info():
     current_time = datetime.now(pytz.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
     return (
-        "email:" "nosikesamuel1@gmail.com\n"
-        "current_datetime:" f"{current_time}\n"
-        "github_url:" "https://github.com/Samcodio/hng_stage-zero"
+        "email": "nosikesamuel1@gmail.com", 
+        "current_datetime": current_time, 
+        "github_url": "https://github.com/Samcodio/hng_stage-zero"
     )
 
 

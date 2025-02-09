de74eb663eba1de0aac72ca42fd5079ddeaf2521
Sam_El <146867369+Samcodio@users.noreply.github.com>
2025-02-09 17:05:17 +0100

    Update data.py

--- data.py
+++ data.py
@@ -10,10 +10,10 @@ setup_cors(app)
 @app.get("/")
 def get_info():
     current_time = datetime.now(pytz.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
-    return (
+    return {
         "email": "nosikesamuel1@gmail.com", 
         "current_datetime": current_time, 
         "github_url": "https://github.com/Samcodio/hng_stage-zero"
-    )
+        }
 
 
